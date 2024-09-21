0d7ccd8c1e8f784ba83f7717e39572d825f44d68
PranayShelar26 <163673119+PranayShelar26@users.noreply.github.com>
2024-09-21 23:17:42 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -46,7 +46,7 @@ const UserSchema = mongoose.Schema({
 const User = mongoose.model('users', UserSchema);
 
 // POST route to create a new user
-app.post('/api/users', async (req, res) => {
+app.post('/', async (req, res) => {
     const { username, email, message } = req.body; // Ensure req.body is correctly parsed
 
     if (!username || !email || !message) {
